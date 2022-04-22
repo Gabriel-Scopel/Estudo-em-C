c0d088fd9f7f53f4f262cffdd97df470a3dc197e
Gabriel Scopel <gabriel.souza.scopel@hotmail.com>
2022-04-22 17:11:13 -0300

    Update strings.c

--- strings.c
+++ strings.c
@@ -163,3 +163,124 @@ void inverteMaiusMinus(char *s,int n){
     
   }
 }
+
+/* Exercício 5
+Leia uma string com tamanho máximo 60 e defina uma função com assinatura
+
+void inverte (char *orig, int n)
+
+que inverte a string.
+
+Teste a função fazendo uma chamada dentro da função "main".
+
+Obs.: Não utilize nenhuma biblioteca adicional.
+
+
+Exemplo:
+
+Entrada:                             Saída:
+
+!@wesDFa+-3                     3-+aFDsew@!  */
+
+#include <stdio.h> 
+#include <stdlib.h>
+
+void inverte (char *, int );
+
+int main() {
+  int n=0;
+  char orig[61]={};
+  fgets(orig,61,stdin);
+  for (int i=0;i<61;i++){
+    if(orig[i]!='\0'){
+      n+=1;
+    }
+  }
+  inverte(orig, n);
+}
+void inverte (char *orig, int n){
+  for(int i=n; i>=0 ;i--){
+    if(orig[i]!='\0' && orig[i] != '\n'){
+      printf("%c", orig[i]);
+    }
+  }
+  
+}
+
+/* Exercício 6
+Leia duas strings com tamanho máximo 50 e defina funções que calculem a soma dos números de cada string. 
+
+Teste a função fazendo uma chamada dentro da função "main".
+
+Obs.: Não utilize nenhuma biblioteca adicional.
+
+
+Exemplo:
+
+Entrada:                             Saída:
+
+ab2dc1*cU                          371             (21 + 350 = 371) 
+
+TR$3uty5Yg0y   */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+  char str1[51]={};
+  fgets(str1,51,stdin);
+  char str2[51]={};
+  char r1[51]={};
+  char r2[51]={};
+  fgets(str2,51,stdin);
+  for (int i=0;i<51;i++){
+    if(str1[i]<='9' && str1[i]>='0' && str1[i]!='\0'){
+      r1[i]= (int)str1[i];
+    }
+  }
+ for (int i=0;i<51;i++){
+    if(str2[i]<='9' && str2[i]>='0' && str2[i]!='\0'){
+      r2[i]=str2[i];
+    }
+   }
+  }
+
+/* exercício 7
+Implemente um programa denominado combinador, que recebe duas strings e deve combiná-las, alternando as letras de cada string, começando com a primeira letra da primeira string, seguido pela primeira letra da segunda string, em seguida pela segunda letra da primeira string, e assim sucessivamente. 
+As letras restantes da cadeia mais longa devem ser adicionadas ao fim da string resultante e retornada.
+
+
+Obs.: Não utilize nenhuma biblioteca adicional.
+
+
+Exemplo:
+
+Entrada:                             Saída:
+
+Tpo oCder                           TopCoder                  
+
+aa bb                                   abab
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+void combinador(char *, char *);
+
+int main() {
+  char str1[51]={};
+  fgets(str1,51,stdin);
+  char str2[51]={};
+  fgets(str2,51,stdin);
+  combinador(str1,str2);
+}
+void combinador(char *str1,char *str2){
+  for( int i=0; i<51; i++){
+    if(str1[i]!='\0' && str1[i]!='\n'){
+      printf("%c", str1[i]);
+    }
+    if((str2[i]!='\0' && str2[i]!='\n'))
+    {
+      printf("%c", str2[i]);
+    }
+  }
+}
