0fda99c3774d12e85bf11b54dbb7f91dd1735069
Gabriel Scopel <gabriel.souza.scopel@hotmail.com>
2022-05-05 20:13:22 -0300

    Update recursão.c

--- recursão.c
+++ recursão.c
@@ -44,3 +44,43 @@ double Pot(int num,int exp){
   
   return 1; 
 }
+
+/* Exercício 3
+Série Harmônica
+
+Implemente, de forma recursiva, a série harmônica:
+
+{\displaystyle \sum _{k=1}^{\infty }{\frac {1}{k}}=1+{\frac {1}{2}}+{\frac {1}{3}}+{\frac {1}{4}}+\cdots }
+
+
+Utilizar recursão, dupla precisão e 4 casas decimais depois da vírgula.
+
+Exemplo:
+
+Entrada                                                     Saída:
+1                                                                1
+10                                                              2.9290 */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+
+double sh(int);
+
+int main(void) {
+  int n;
+  scanf("%d", &n);
+  sh(n);
+}
+double sh(int n){
+  double a=1;
+  
+  double valor=0;
+  while(n>0){
+    double valor=valor+(a/n);
+    n--;
+    printf("%lf\n", valor);
+  }
+  return 0;
+}
